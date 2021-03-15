b0afe0b7680d1cce5a84bfdc7ec07d8efecbd4af
Ilya <ilya.kolt@gmail.com>
2021-03-16 00:25:07 +0300

    first commit

---
+++ BFS.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BFS_2
+{
+    public class BFS
+    {
+        public Queue<int> queue = new Queue<int>();
+
+        public string search(Node node, int value)
+        {
+            int queueElement;
+            if (node.Visited == false)
+            {
+                node.Visited = true;
+                queue.Enqueue(node.Count);
+                if (queue != null)
+                {
+                    queueElement = queue.Dequeue();
+                    if (queueElement == value)
+                    {
+                        return("Элемент найден, его имя " + node.Name);
+                    }
+                    else
+                        return search(node.Edges[0].Node,value);
+                }
+                else
+                {
+                    return ("Очередь пуста");
+                }
+            }
+            else
+                return ("Visited");
+        }
+    }
+}
